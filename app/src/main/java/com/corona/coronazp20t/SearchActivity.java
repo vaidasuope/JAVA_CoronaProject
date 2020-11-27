9395871a10cd12fbc5209448d6308461ea233aed
vaidasuope <vaida.dabasinskaite@gmail.com>
2020-11-27 11:15:31 +0200

    added remember me, fixed RegisterLoginActivity, with error

--- app/src/main/java/com/corona/coronazp20t/SearchActivity.java
+++ app/src/main/java/com/corona/coronazp20t/SearchActivity.java
@@ -4,11 +4,34 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+
+
 public class SearchActivity extends AppCompatActivity {
+
+    //apsirasom URL adresa
+    private static final String URL="https://covid19-api.weedmark.systems/api/v1/stats";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search);
+
+        try {
+            readJSONFromAPI(URL);
+        } catch (IOException e) {
+            System.out.println("Įvyko įvedimo - išvedimo klaida");//i ekrana pranesima isveda
+            System.out.println("Daugiau informacijos:" + e.getMessage());//get Message isves i ekrana klaida kokia ivyko
+        } catch (JSONException e) {
+            System.out.println("Nuskaitant JSON įvyko klaida");//i ekrana pranesima isveda
+            System.out.println("Daugiau informacijos:" + e.getMessage());
+        }
+    }
+    public static void readJSONFromAPI(String URL) throws IOException, JSONException {
+        JSONObject json = JSON.readJsonFromUrl(URL);//JSON kreipiames i klase, kur JSON apsirase turim
+        System.out.println(json.toString());
     }
 }
